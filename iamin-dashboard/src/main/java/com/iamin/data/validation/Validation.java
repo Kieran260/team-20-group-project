a24c5961854548f21ee57963e5ee1efc1e4d02f4
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-03-19 09:30:20 +0000

    New validation functions and tests

--- iamin-dashboard/src/main/java/com/iamin/data/validation/Validation.java
+++ iamin-dashboard/src/main/java/com/iamin/data/validation/Validation.java
@@ -1,28 +1,44 @@
 package com.iamin.data.validation;
 
+import org.jsoup.Jsoup;
+import com.iamin.data.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.jsoup.safety.Safelist;
+
+@Component
 public class Validation {
+    private final LoginService loginService;
+
+    @Autowired
+    public Validation(LoginService loginService) {
+        this.loginService = loginService;
+    }
 
     // Checks a character is alphanumeric
-    public static boolean isAlphaNumeric(char char1) {
+    public boolean isAlphaNumeric(char char1) {
         return (char1 >= 'a' && char1 <= 'z') || (char1 >= 'A' && char1 <= 'Z') || (char1 >= '0' && char1 <= '9');
     }
     
     // Check that username does not already exist in table
     // Check that length is 8 characters and alphanumeric
-    public static boolean userNameValidation(String username) {
-
-        int usernameLength = username.length();
-
-        // TODO: check username not already in table 
-
-        // Check length
-        if (usernameLength != 8) {
+    public boolean userNameValidation(String username) {
+        
+        if (loginService.checkIfUsernameExists(username)) {
             return false;
         }
 
+        // Check length
+        if (username.length() != 8) {
+            return false;
+        }
+        // Check for SQL injection
+        if (isSqlInjection(username)) {
+            return false;
+        }
         // Check alphanumeric
         else {
-            for (int i = 0; i < usernameLength; i++) {
+            for (int i = 0; i < username.length(); i++) {
                 if (!isAlphaNumeric(username.charAt(i))) {
                     return false;
                 }
@@ -31,18 +47,14 @@ public class Validation {
         return true;
     }
 
-
     // Check password is 8+ characters and contains one number
     // Check confirmPassword is the same as password
-    public static boolean passwordValidation(String password, String confirmPassword) {
-
-        int passwordLength = password.length();
+    public boolean passwordValidation(String password, String confirmPassword) {
         
-        if (passwordLength < 8) {
+        if (password.length() < 8) {
             return false;
-        }
-        else {
-            for (int i = 0; i < passwordLength; i++) {
+        } else {
+            for (int i = 0; i < password.length(); i++) {
                 if (Character.isDigit(password.charAt(i))) {
                     if (password.equals(confirmPassword)) {
                         return true;
@@ -52,4 +64,31 @@ public class Validation {
             return false;
         }
     }
+    
+    // Checks for SQL injections
+    public boolean isSqlInjection(String message) {
+        // Common SQL injection keywords
+        String[] sqlKeywords = {"SELECT", "INSERT", "UPDATE", "DELETE", "CREATE", "DROP", "ALTER", "EXECUTE", "TRUNCATE"};
+            
+        // Loop through the message and check for SQL injection keywords
+        for (String keyword : sqlKeywords) {
+            if (message.toUpperCase().contains(keyword)) {
+                return true;
+            }
+        }
+            
+        // No SQL injection keywords found
+        return false;
+    }
+
+    // Function to sanitize inputs
+    public String sanitizeInput(String input) {
+        // Remove any HTML tags and attributes from the input using Jsoup
+        String sanitized = Jsoup.clean(input, Safelist.none());
+        // Return the sanitized input
+        return sanitized;
+    }
+    
+
+    
 }

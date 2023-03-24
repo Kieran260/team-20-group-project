ddedb511cf303c6b00369d7c5b116d68d61ec979
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-03-24 14:02:56 +0000

    SQL test data

--- iamin-dashboard/src/main/resources/data.sql
+++ iamin-dashboard/src/main/resources/data.sql
@@ -5,8 +5,20 @@ INSERT INTO sample_person(id, version, first_name, last_name) VALUES (1, 1, 'Joh
 INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (1, 1, 1, '09:00:00', '17:00:00', '2023-03-07');
 INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (2, 1, 1, '09:15:00', '17:30:00', '2023-03-08');
 INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (3, 1, 1, '08:45:00', '16:45:00', '2023-03-09');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (4, 1, 1, '09:00:00', '17:00:00', '2023-03-10');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (5, 1, 1, '09:15:00', '17:30:00', '2023-03-11');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (6, 1, 1, '08:45:00', '16:45:00', '2023-03-12');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (7, 1, 1, '09:00:00', '17:00:00', '2023-03-13');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (8, 1, 1, '09:15:00', '17:30:00', '2023-03-14');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (9, 1, 1, '08:45:00', '16:45:00', '2023-03-15');
 
-INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (4, 1, 1, '09:00:00', '17:00:00', '2023-02-07');
-INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (5, 1, 1, '09:15:00', '17:30:00', '2023-02-08');
-INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (6, 1, 1, '09:15:00', '17:30:00', '2023-02-09');
-INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (7, 1, 1, '09:15:00', '17:30:00', '2023-02-10');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (10, 1, 1, '09:00:00', '17:00:00', '2023-02-16');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (11, 1, 1, '09:15:00', '17:30:00', '2023-02-17');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (12, 1, 1, '09:15:00', '17:30:00', '2023-02-18');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (13, 1, 1, '09:15:00', '17:30:00', '2023-02-19');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (14, 1, 1, '09:00:00', '17:00:00', '2023-02-07');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (15, 1, 1, '09:15:00', '17:30:00', '2023-02-08');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (16, 1, 1, '09:15:00', '17:30:00', '2023-02-09');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (17, 1, 1, '09:15:00', '17:30:00', '2023-02-10');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (19, 1, 1, '09:15:00', '17:30:00', '2023-02-11');
+INSERT INTO check_in_out(id, version, person_id, check_in_time, check_out_time, date) VALUES (20, 1, 1, '09:15:00', '17:30:00', '2023-02-12');

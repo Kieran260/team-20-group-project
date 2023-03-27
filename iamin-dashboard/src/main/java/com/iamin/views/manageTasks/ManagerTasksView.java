fe9260c3698c6e1d32a4c583556fc2657fa801e3
Mason Booth <boothm@talktalk.net>
2023-03-27 13:39:55 +0100

    Pushed for Karam to add view to sidebar

---
+++ iamin-dashboard/src/main/java/com/iamin/views/manageTasks/ManagerTasksView.java
@@ -0,0 +1,12 @@
+package com.iamin.views.manageTasks;
+
+@PageTitle("Manage Tasks")
+@Route(value = "manage-tasks", layout = MainLayout.class)
+@RolesAllowed("ADMIN")
+
+public class ManagerTasksView {
+
+    public ManagerTasksView() {
+
+    }
+}

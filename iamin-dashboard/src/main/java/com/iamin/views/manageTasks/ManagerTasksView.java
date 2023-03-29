cd611a6876237008e0f4bffb128a139edfdcc707
Mason Booth <boothm@talktalk.net>
2023-03-29 19:17:47 +0100

    Ammended design

--- iamin-dashboard/src/main/java/com/iamin/views/manageTasks/ManagerTasksView.java
+++ iamin-dashboard/src/main/java/com/iamin/views/manageTasks/ManagerTasksView.java
@@ -2,16 +2,14 @@ package com.iamin.views.manageTasks;
 
 import com.iamin.data.entity.Tasks;
 import com.iamin.views.MainLayout;
-import com.vaadin.flow.component.UI;
+
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.component.dependency.Uses;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -29,22 +27,20 @@ public class ManagerTasksView extends HorizontalLayout{
 
     Grid<Tasks> grid = new Grid<>(Tasks.class, false);
 
+    SplitLayout splitLayout = new SplitLayout();
+
     public ManagerTasksView() {
         addClassName("list-view");
-        
+
         configureTasks();
         configureAssignBar();
+        add(splitLayout);
     }
 
     
     public void configureTasks() {
         VerticalLayout tasks = new VerticalLayout();
         tasks.setWidth("800px");
-        tasks.setHeightFull();
-
-        // set title
-        Button title = new Button("Current Tasks");
-        tasks.add(title);
 
         // give grid a class name - as a referece point
         grid.addClassName("manager-assigns-tasks");
@@ -59,31 +55,31 @@ public class ManagerTasksView extends HorizontalLayout{
 
         tasks.add(grid);
 
-        add(tasks);
+        splitLayout.addToPrimary(tasks);
     }
 
 
     public void configureAssignBar() {
         VerticalLayout content = new VerticalLayout();
         content.setWidth("200px");
-        content.setHeightFull();
 
         // TOP
-        TextField selectEmployee = new TextField();
+        Select<String> selectEmployee = new Select<>();
         selectEmployee.setLabel("Select Employee");
         TextField selectText = new TextField();
-        selectText.setLabel("Select Task");
-        DatePicker dueDate = new DatePicker("Due Date");
+        selectText.setLabel("Task Description");
+        DatePicker dueDate = new DatePicker("Deadline");
         Button button = new Button("Assign Task");
         button.setWidthFull();
 
         // BOTTOM
-        Button progress = new Button("% Assigned A Task");
+        Div progressBarLabel = new Div();
+        progressBarLabel.setText("% Assigned A Task");
         ProgressBar progressBar = new ProgressBar();
         progressBar.setValue(0.5);
 
-        content.add(selectEmployee, selectText, dueDate, button, progress, progressBar);
+        content.add(selectEmployee, selectText, dueDate, button, progressBarLabel, progressBar);
 
-        add(content);
+        splitLayout.addToSecondary(content);
     }
 }

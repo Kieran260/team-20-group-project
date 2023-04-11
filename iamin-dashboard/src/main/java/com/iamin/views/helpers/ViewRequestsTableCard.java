a3e7811fd8adff8c0a38875ef51b3441c54a392d
JSkells0 <jamieskellorn@hotmail.co.uk>
2023-04-11 16:21:51 +0100

    replace table icons with images to see if this fixes rendering bug - it does not

--- iamin-dashboard/src/main/java/com/iamin/views/helpers/ViewRequestsTableCard.java
+++ iamin-dashboard/src/main/java/com/iamin/views/helpers/ViewRequestsTableCard.java
@@ -2,13 +2,18 @@ package com.iamin.views.helpers;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 
 import java.util.List;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.button.Button;
 import java.util.ArrayList;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.grid.Grid.Column;
 import com.vaadin.flow.component.grid.dataview.GridListDataView;
 
 // Handle absence and holiday in separate tables?
@@ -17,27 +22,36 @@ import com.vaadin.flow.component.grid.dataview.GridListDataView;
 public class ViewRequestsTableCard {
 
     public Div createCard() {
-        // TODO: Populate "requests" with absences/holidays
-        // Order table by earliest start date
-
         // Dummy data
-        List<Request> requests = new ArrayList<Request>();
-        Request dummyReq = new Request("John", "Doe",
-                "12-03-23", "18-03-23", "Fishing", "Yes");
+        List<Request> activeRequestsList = new ArrayList<>();
+        List<Request> pastRequestsList = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            requests.add(dummyReq);
+        Request dummyReq = new Request("John", "Doe",
+                "12-03-23", "18-03-23", "Fishing", true);
+
+        Request dummyReq2 = new Request("John", "Doe",
+                "12-03-23", "18-03-23", "frolicking", false);
+
+        for (int i = 0; i < 5; i++) {
+            activeRequestsList.add(dummyReq);
+            pastRequestsList.add(dummyReq);
+        }
+        for (int i = 0; i < 5; i++) {
+            activeRequestsList.add(dummyReq2);
+            pastRequestsList.add(dummyReq2);
         }
 
-        ListDataProvider<Request> dataProvider = new ListDataProvider<>(requests);
+        // Create the data providers for each table
+        ListDataProvider<Request> activeRequestsProvider = new ListDataProvider<>(activeRequestsList);
+        ListDataProvider<Request> pastRequestsProvider = new ListDataProvider<>(pastRequestsList);
 
         // Create the tables and set their data providers
         Grid<Request> activeRequests = new Grid<>();
-        activeRequests.setDataProvider(dataProvider);
+        activeRequests.setDataProvider(activeRequestsProvider);
         activeRequests.setWidth("100%");
 
         Grid<Request> pastRequests = new Grid<>();
-        pastRequests.setDataProvider(dataProvider);
+        pastRequests.setDataProvider(pastRequestsProvider);
         pastRequests.setWidth("100%");
 
         // Create table headers
@@ -62,20 +76,38 @@ public class ViewRequestsTableCard {
         container.setWidth("100%");
         container.setHeight("50%");
 
+        ComponentRenderer<Image, ViewRequestsTableCard.Request> approvedRendererA = new ComponentRenderer<>(request -> {
+            if (request.isApproved()) {
+                return new Image("approved.png", "approved");
+            } else {
+                return new Image("rejected.png", "rejected");
+            }
+        });
+
+        ComponentRenderer<Image, ViewRequestsTableCard.Request> approvedRendererP = new ComponentRenderer<>(request -> {
+            if (request.isApproved()) {
+                return new Image("approved.png", "approved");
+            } else {
+                return new Image("rejected.png", "rejected");
+            }
+        });
+
         // Add columns to the tables
+
         activeRequests.addColumn(Request::getFirstName).setHeader("First Name");
         activeRequests.addColumn(Request::getLastName).setHeader("Last Name");
         activeRequests.addColumn(Request::getStartDate).setHeader("Start date");
         activeRequests.addColumn(Request::getEndDate).setHeader("End date");
         activeRequests.addColumn(Request::getReason).setHeader("Reason");
-        activeRequests.addColumn(Request::getApproved).setHeader("Approved");
+        activeRequests.addColumn(approvedRendererA).setHeader("Approved");
+        // need images in resources
 
         pastRequests.addColumn(Request::getFirstName).setHeader("First Name");
         pastRequests.addColumn(Request::getLastName).setHeader("Last Name");
         pastRequests.addColumn(Request::getStartDate).setHeader("Start date");
         pastRequests.addColumn(Request::getEndDate).setHeader("End date");
         pastRequests.addColumn(Request::getReason).setHeader("Reason");
-        pastRequests.addColumn(Request::getApproved).setHeader("Approved");
+        pastRequests.addColumn(approvedRendererP).setHeader("Approved");
 
         container.add(activeHeader, activeRequests, pastHeader, pastRequests);
         return container;
@@ -87,10 +119,10 @@ public class ViewRequestsTableCard {
         private final String startDate;
         private final String endDate;
         private final String reason;
-        private final String approved;
+        private final boolean approved;
 
         public Request(String firstName, String lastName, String startDate,
-                String endDate, String reason, String approved) {
+                String endDate, String reason, Boolean approved) {
 
             this.firstName = firstName;
             this.lastName = lastName;
@@ -98,7 +130,6 @@ public class ViewRequestsTableCard {
             this.endDate = endDate;
             this.reason = reason;
             this.approved = approved;
-
         }
 
         public String getFirstName() {
@@ -121,8 +152,9 @@ public class ViewRequestsTableCard {
             return reason;
         }
 
-        public String getApproved() {
+        public Boolean isApproved() {
             return approved;
         }
     }
+
 }

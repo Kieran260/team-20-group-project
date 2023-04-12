af1cd292da6b4bb4eeebe61212f287f82ad7a1b9
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-04-12 11:34:46 +0100

    Implemented timetable content

--- iamin-dashboard/src/main/java/com/iamin/views/dashboard/DashboardView.java
+++ iamin-dashboard/src/main/java/com/iamin/views/dashboard/DashboardView.java
@@ -81,6 +81,7 @@ public class DashboardView extends VerticalLayout {
 
     @Autowired
     private final DepartmentMembersCard departmentMembersCard;
+    
     @Autowired
     PasswordEncoder passwordEncoder;
 

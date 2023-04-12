94ce5ac3665ad9a206b82fee837a9a8d26cc83bb
Khaled052 <khaledaliessa@gmail.com>
2023-04-12 03:05:22 +0300

    add cross and tick for approved column

--- iamin-dashboard/src/main/java/com/iamin/views/helpers/ViewRequestsTableCard.java
+++ iamin-dashboard/src/main/java/com/iamin/views/helpers/ViewRequestsTableCard.java
@@ -1,15 +1,18 @@
 package com.iamin.views.helpers;
-
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.data.provider.ListDataProvider;
-
-import java.util.List;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.button.Button;
 import java.util.ArrayList;
+import java.util.List;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.grid.dataview.GridListDataView;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.component.Component;
+
 
 // Handle absence and holiday in separate tables?
 // Pull records from DB and create Request object with important details?
@@ -25,9 +28,12 @@ public class ViewRequestsTableCard {
         Request dummyReq = new Request("John", "Doe",
                 "12-03-23", "18-03-23", "Fishing", "Yes");
 
-        for (int i = 0; i < 10; i++) {
-            requests.add(dummyReq);
-        }
+                for (int i = 0; i < 10; i++) {
+                    String approved = (i % 2 == 0) ? "Yes" : "No";
+                    requests.add(new Request("John", "Doe", "12-03-23", "18-03-23", "Fishing", approved));
+                }
+                
+                
 
         ListDataProvider<Request> dataProvider = new ListDataProvider<>(requests);
 
@@ -68,20 +74,32 @@ public class ViewRequestsTableCard {
         activeRequests.addColumn(Request::getStartDate).setHeader("Start date");
         activeRequests.addColumn(Request::getEndDate).setHeader("End date");
         activeRequests.addColumn(Request::getReason).setHeader("Reason");
-        activeRequests.addColumn(Request::getApproved).setHeader("Approved");
-
+        activeRequests.addColumn(new ComponentRenderer<>(Request::getApprovedComponent)).setHeader("Approved");
+        
+             
         pastRequests.addColumn(Request::getFirstName).setHeader("First Name");
         pastRequests.addColumn(Request::getLastName).setHeader("Last Name");
         pastRequests.addColumn(Request::getStartDate).setHeader("Start date");
         pastRequests.addColumn(Request::getEndDate).setHeader("End date");
         pastRequests.addColumn(Request::getReason).setHeader("Reason");
-        pastRequests.addColumn(Request::getApproved).setHeader("Approved");
+        pastRequests.addColumn(new ComponentRenderer<>(Request::getApprovedComponent)).setHeader("Approved");
 
         container.add(activeHeader, activeRequests, pastHeader, pastRequests);
         return container;
     }
 
     private static class Request {
+        public Component getApprovedComponent() {
+            Icon icon;
+            if ("Yes".equals(approved)) {
+                icon = new Icon(VaadinIcon.CHECK);
+                icon.getStyle().set("color", "green");
+            } else {
+                icon = new Icon(VaadinIcon.CLOSE);
+                icon.getStyle().set("color", "red");
+            }
+            return icon;
+        }
         private final String firstName;
         private final String lastName;
         private final String startDate;
@@ -125,4 +143,6 @@ public class ViewRequestsTableCard {
             return approved;
         }
     }
+    
+    
 }
